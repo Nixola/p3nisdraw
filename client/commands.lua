8c20d13aabcf8e23f98b7f2b1b8e6389ba4b8a52
Nixola <niki.or97@yahoo.it>
2017-11-27 11:35:42 +0100

    Added circled star property

--- client/commands.lua
+++ client/commands.lua
@@ -3,16 +3,28 @@ local commands = {}
 commands.star = function(radius, points, circled)
 	points = tonumber(points)
 	radius = tonumber(radius)
+	local even
 	if not (points and radius) then return end
 	if (points <= 2) then return end
 	local star = {}
 	star.basePoints = {}
+	if points%2 == 0 then
+		--points = points / 2
+		even = true
+	end
 	local n = math.floor(points/2 + 1)
 	for i = 0, points do
 		local a = i * n/points * 2 * math.pi
 		star.basePoints[i*2+1] = math.sin(a) * radius
 		star.basePoints[i*2+2] = -math.cos(a) * radius
 		print(i, star.basePoints[i*2+1], star.basePoints[i*2+2])
+	end
+	if circled then
+		for i = 1, radius*2 do
+			local a = i / radius * math.pi
+			star.basePoints[(i + points) * 2 + 1] = math.sin(a) * radius
+			star.basePoints[(i + points) * 2 + 2] = -math.cos(a) * radius
+		end
 	end
 	star.mousemoved = function(self, x, y, dx, dy)
 		for i = 1, #self.basePoints/2 do
