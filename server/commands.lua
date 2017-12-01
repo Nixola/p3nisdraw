530fd3f967fba3c1d69f801d2bf54ca8e725647f
Nixola <niki.or97@yahoo.it>
2017-12-01 11:28:43 +0100

    Added a Lua server command

--- server/commands.lua
+++ server/commands.lua
@@ -23,4 +23,13 @@ commands.unban = function(ip)
 	bans[ip] = nil
 end
 
+commands.lua = function(...)
+	local str = table.concat({...}, " ")
+	local f, e = loadstring(str)
+	if not f then print("Error loading:", e) return end
+	local r, e = pcall(f)
+	if not r then print("Error executing:", e) return end
+	print(e)
+end
+
 return commands
