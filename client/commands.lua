c0fd7eafb8961556ab0181b80db1589830284a88
Nixola <niki.or97@yahoo.it>
2018-05-02 20:51:54 +0200

    Added client-side Lua command

--- client/commands.lua
+++ client/commands.lua
@@ -1,4 +1,10 @@
 local commands = {}
 
+commands.lua = function(...)
+	local f, e = loadstring(table.concat({...}, " "))
+	if not f then print(e) return end
+	local r, e = pcall(f)
+	if not r then print(e) return end
+end
 
 return commands
